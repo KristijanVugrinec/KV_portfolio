0455b9bda233ccba6d8876c8e74aa4ac430cbd24
Kristijan Vugrinec <kristijan.vugrinec2@gmail.com>
2024-05-21 20:12:09 +0200

    creating header and adding logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom'
+import KVLogo from '../../public/kvlogo.svg?react'
+
+const headerLinks = [
+  {
+    title:"about",
+    href: "/",
+  },
+  {
+    title:"contacts",
+    href: "/hehe",
+  }
+]
+
+function Header() {
+return (
+  <>
+  <header
+     style={{
+    //     color: "white",
+    //     fontSize: 24,
+         position: "fixed",
+         top: 50,
+         left: 0,
+         width: "100%",
+         display: "flex",
+    //     flexWrap: "wrap",
+    //     gap: 34,
+         justifyContent: "center",
+         alignItems: "center",
+    //     padding: 10,
+     }}
+    ><div style={{ position: "absolute", left: "50%", transform: "translateX(-50%)" }}>
+    <KVLogo />
+    </div>
+    <div style={{position: "absolute", right: "90px", display: "flex", backgroundColor: "transparent"}}>
+    {headerLinks.map((link) => {
+        const { title, href } = link;
+        return (
+            <Link
+            key={href}
+            to={href}
+            title={title}
+            style={{
+                backgroundColor: "transparent",
+                marginLeft: 50,
+                fontWeight: "normal",
+                fontSize: 24,
+            }}
+            >
+          {title}
+        </Link>
+      );
+    })}
+    </div>
+  </header>
+</>
+)
+}
+
+export default Header
