e40df8d5ae9882347253ad315cba022dc463daaa
Kristijan Vugrinec <kristijan.vugrinec2@gmail.com>
2024-05-24 11:36:41 +0200

    All component added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import Header from './components/Header'
 import AboutMe from './components/AboutMe'
 import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { Experience } from './components/Experience'
+import SkillsText from './components/Skills'
 
 function App() {
 return (
   <>
+   <div style={{position:"fixed", width:"100%", height:"100%",opacity:"30%",pointerEvents:"none", left:0, top:0,}}>
+  <Canvas style={{pointerEvents:"none"}}>
+    <Experience />
+  </Canvas>
+  </div> 
   <Header/>
-  <AboutMe />
+  <AboutMe /> 
+  <div style={{position:"relative",marginTop:"15rem"}}>
+    <SkillsText
+    text="SKILLS"
+    startIndex={3}
+    endIndex={5}
+    highlightStyle={{color: '#44DDC0'}} />
+  </div> 
+
 </>
 )
 }
