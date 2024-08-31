073f59fee8cba513ee1cb29a987271bc6d298793
Kristijan Vugrinec <kristijan.vugrinec2@gmail.com>
2024-08-31 18:10:59 +0200

    Added WeatherApp to projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import style from "./Projects.module.css"
 import { useState,useEffect } from "react"
 import { useInView } from "react-intersection-observer"
+import weatherApp from './projects/WeatherApp.png'
 
 
 function Projects () {
@@ -23,7 +24,13 @@ return (
 <p className={`${style.title} ${inView ? style.visible : style.hidden}`}>PROJECTS</p>
     </div>
 <div className={`${style.box} ${inView ? style.visible : style.hidden} `}>
-    <p className={style.text}>{text}</p>
+    <div className={style.weatherApp}>
+        <img src={weatherApp} className={style.weatherAppPicture} />
+        <div className={style.description}>
+        <p><a href="https://instantweathernow.netlify.app" target="_blank">Weather App</a></p>
+        <p>Simple weather app providing accurate, up-to-date weather for the current day.</p>
+        </div>
+    </div>
 </div>
 </div>
 )
