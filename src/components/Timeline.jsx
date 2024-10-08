d86420384abdbcb8bd0c94cece51ba96fc763842
Kristijan Vugrinec <kristijan.vugrinec2@gmail.com>
2024-10-08 02:31:23 +0200

    Added Timeline section(need to update)

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,49 @@
+import style from "./Timeline.module.css"
+
+
+const Timeline = () => {
+    return (
+        <>
+        <div className={style.timelinemain}>
+        <div className={style.line}>
+            </div>
+            <div>
+                <p>
+                    2023
+                </p>
+            </div>
+            <div className={style.line}>
+            </div>
+            <div>
+                <p>
+                    2023
+                </p>
+            </div>
+            <div className={style.line}>
+            </div>
+            <div>
+                <p>
+                    2023
+                </p>
+            </div>
+            <div className={style.line}>
+            </div>
+            <div>
+                <p>
+                    2024
+                </p>
+            </div>
+            <div className={style.line}>
+            </div>
+            <div>
+                <p>
+                    2024
+                </p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+
+export default Timeline
